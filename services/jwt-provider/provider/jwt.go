3767a59c59e6833f8b953c23dc4c6b7163bd87e7
Rejjie <rejjie@foxmail.com>
2023-12-02 23:29:16 +0800

    try to switch the cache to fishMap

--- services/jwt-provider/provider/jwt.go
+++ services/jwt-provider/provider/jwt.go
@@ -69,11 +69,11 @@ func (p *jwtProvider) CreateJWTFromIP(ctx context.Context, ipAddress string, cla
 	gatewayPrefix := strings.Join(ipElms, ".")
 	//fmt.Sprintf("%s", p.fishMap[gatewayPrefix])
 	//
-	//res, err := sec.CreateBotJWT(p.fishMap[gatewayPrefix], bot, claims, p.jwtCreatorFunc)
-	for k, v := range p.GetScannerMap(ctx) {
-		log.WithField("api", "handleJwtRequest").Infof("ScannerMap CacheElm - [%s] - [%s] - [%s] - [%s]", k, v, p.fishMap[gatewayPrefix], p.key)
-	}
-	res, err := sec.CreateBotJWT(p.key, bot, claims, p.jwtCreatorFunc)
+	res, err := sec.CreateBotJWT(p.fishMap[gatewayPrefix], bot, claims, p.jwtCreatorFunc)
+	//for k, v := range p.GetScannerMap(ctx) {
+	//	log.WithField("api", "handleJwtRequest").Infof("ScannerMap CacheElm - [%s] - [%s] - [%s] - [%s]", k, v, p.fishMap[gatewayPrefix], p.key)
+	//}
+	//res, err := sec.CreateBotJWT(p.key, bot, claims, p.jwtCreatorFunc)
 	if err != nil {
 		logger.WithError(err).Error("error creating jwt")
 		return "", err
