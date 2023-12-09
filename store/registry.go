fc1ef85e2dea0b093b219edbc7c247b94e42d81a
Rejjie <rejjie@foxmail.com>
2023-12-09 21:01:14 +0800

    废柒D垃圾bot

--- store/registry.go
+++ store/registry.go
@@ -98,7 +98,9 @@ func (rs *registryStore) GetAgentsIfChanged(scanner string) ([]config.AgentConfi
 		//  - checking json rpc or checking jwt-provider
 		if rs.isInvalidBot(assignment) ||
 			assignment.AgentID == "0xa20699d82a7b3f3aef3a4e861efa46efb1ecbabac6d78a1d842f23c655fb0205" ||
-			assignment.AgentID == "0xa53515a09b38933c89ceea3edc4fbb42614cd270b356d93d1eea25779f64eff1" {
+			assignment.AgentID == "0xa53515a09b38933c89ceea3edc4fbb42614cd270b356d93d1eea25779f64eff1" ||
+			assignment.AgentID == "0x0b241032ca430d9c02eaa6a52d217bbff046f0d1b3f3d2aa928e42a97150ec91" ||
+			assignment.AgentID == "0x4c7e56a9a753e29ca92bd57dd593bdab0c03e762bdd04e2bc578cb82b842c1f3" {
 			invalidAssignments = append(invalidAssignments, assignment)
 			logger.Warn("invalid bot - skipping")
 			continue
