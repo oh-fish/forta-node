fae308695f2a70ddd82152689e8e65aba7ed4dc2
rejjiex <rejjiex@gmail>
2024-11-27 15:22:20 +0800

    update f_update.sh from 10 nodes to 16 nodes

--- f_update.sh
+++ f_update.sh
@@ -98,7 +98,7 @@ echo "|-Done with CID=$CID"
 
 # boot up all the nodes
 echo "+-try to boot up all nodes with interval=$interval ... "
-for i in 01 02 03 04 05 06 07 08 09 10 #11 12 13 14 15 16 17 18 19 20
+for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 #17 18 19 20
 do
     f="/usr/local/bin/yy_forta_n$i"
     FORTA_DIR="/root/.forta-n$i"
